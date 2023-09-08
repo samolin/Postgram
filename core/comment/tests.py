db0cf523b7266af0a731fde76250fcdbb75abe27
samolin <samolin.vlad@gmail.com>
2023-09-08 21:23:20 +0300

    Add tests or user, post, autoflake does not go through tests and fixtures

--- core/comment/tests.py
+++ core/comment/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+import pytest
+
+from core.comment.models import Comment
+from core.fixtures.post import post
+from core.fixtures.user import user
+
+
+@pytest.mark.django_db
+def test_create_comment(user, post):
+    comment = Comment.objects.create(
+        author=user, post=post, body="Test Comment Body"
+    )
+    assert comment.author == user
+    assert comment.post == post
+    assert comment.body == "Test Comment Body"
--- core/post/tests.py
+++ core/post/tests.py
@@ -1,1 +1,11 @@
-# Create your tests here.
+import pytest
+
+from core.fixtures.user import user
+from core.post.models import Post
+
+
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(author=user, body="Test Post Body")
+    assert post.body == "Test Post Body"
+    assert post.author == user
